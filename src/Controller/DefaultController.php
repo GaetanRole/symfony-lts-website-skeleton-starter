57e047bc47f1714cb2c0c59b91e49662a401a97d
Gaëtan Rolé-Dubruille <gaetan.role-dubruille@epitech.eu>
2018-11-17 21:59:21 +0100

    Adding php_codesniffer, TimeContinuum component with injected service, updating components and gitignore.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,17 +1,55 @@
 <?php
 
+/**
+ * Default/Index controller file
+ *
+ * PHP Version 7.2
+ *
+ * @category Controller
+ * @package  Default
+ * @author   Gaëtan Rolé-Dubruille <gaetan@wildcodeschool.fr>
+ */
+
 namespace App\Controller;
 
+use App\Service\GlobalClock;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Default controller.
+ *
+ * @category Controller
+ * @package  Default
+ * @author   Gaëtan Rolé-Dubruille <gaetan@wildcodeschool.fr>
+ */
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * Home page
+     *
+     * @Route("/", name="index", methods={"GET", "HEAD"})
+     * @return     Response A Response instance
      */
     public function index()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * A sample method to use GlobalClock service based on TimeContinuum component
+     *
+     * @param GlobalClock $clock Given project's clock to handle all DateTime objects
+     *
+     * @Route("/time-continuum", name="show_time_continuum", methods={"GET", "HEAD"})
+     * @return                   Response A Response instance
+     */
+    public function showTimeContinuumSample(GlobalClock $clock)
+    {
+        return $this->render(
+            'samples/time_continuum.html.twig',
+            ['clock' => $clock]
+        );
     }
 }
